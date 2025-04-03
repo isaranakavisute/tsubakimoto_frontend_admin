e01769b16f38dfefe896d6d178075bacdaaef663
Isara Nakavisute <isara_nakavisute@hotmail.com>
2025-04-03 02:55:56 +0000

    add edit/delete suer screen

--- app/adminpage/userscreen.tsx
+++ app/adminpage/userscreen.tsx
@@ -10,14 +10,15 @@ import axios from 'axios';
 import React from "react";
 import { unstable_cache } from "next/cache";
 //import MainScreen from '../mainpage/userscreen';
-
+import pencil_icon from "./images/pencil-icon.svg";
+import trash_icon from "./images/recycle-bin-icon.svg";
 
 
 export default function UserScreen() {
 
   const router = useRouter;
   const [std_json_res, set_std_json_res] = React.useState([{"Id": "value","usr": "value","pwd": "value","access": "value"}]); //React.useState(null);
-  //var std_json_res;
+ 
 
   function autoload()
   {
@@ -40,13 +41,25 @@ export default function UserScreen() {
     autoload();
   }, []);
 
+  function onEditIconClicked()
+  {
+   alert('Item will be edited');
+  }
+
+  function onDeleteIconClicked()
+  {
+   alert('Item will be deleted');
+  }
+
   return (
         <div className="flex flex-col w-full h-[100vh] bg-white">                 
          <div className="flex h-full w-full flex-row items-center justify-center bg-white">
           <div className="flex h-full w-full bg-white flex-col justify-start items-center rounded-xl">
+
             <div className="flex flex-row w-full h-[5vh] border-0 border-solid border-black items-center justify-center bg-amber-600">
              <div className="text-3xl text-black">Registered user</div>
             </div>
+
             <div className="flex flex-col w-full h-[5vh] border-0 border-solid bg-amber-600 items-center justify-start"> 
              <div className="flex flex-row w-full h-full border-0 border-solid border-black">
               <div className="flex flex-row w-1/5 h-full border-1 border-solid border-black bg-amber-600 justify-center text-black">USER ID</div>
@@ -56,51 +69,69 @@ export default function UserScreen() {
               <div className="flex flex-row w-1/5 h-full border-1 border-solid border-black bg-amber-600 justify-center text-black">ADMIN TOOL</div>
              </div>
             </div>
-            {/* <div className="flex flex-col w-full h-[5vh] bg-white justify-start items-center mb-10"> 
-             <div className="flex flex-row w-9/10 h-full bg-black">
-             </div>
-            </div> */}
+
             <div className="flex flex-col w-full h-full border-1 border-solid border-purple items-center justify-start overflow-y-auto"> 
-            {/* <div className="flex flex-col w-9/10 h-full border-1 border-solid border-yellow items-center justify-center">  */}
-             {/* <table className="border-1 border-solid border-purple w-full h-full">
-             <tr className="border-1 border-solid border-black w-full">
-                  <td className="border-1 border-solid border-black w-1/4"><div>1</div></td>
-                  <td className="border-1 border-solid border-black w-1/4"><div>1</div></td>
-                  <td className="border-1 border-solid border-black w-1/4"><div>1</div></td>
-                  <td className="border-1 border-solid border-black w-1/4"><div>1</div></td>
-             </tr> */}
              {
               std_json_res.map((post) => (
-                // <tr className="border-1 border-solid border-black w-full">
-                //   <td className="border-1 border-solid border-black w-1/4"><div>{post.Id}</div></td>
-                //   <td className="border-1 border-solid border-black w-1/4"><div>{post.usr}</div></td>
-                //   <td className="border-1 border-solid border-black w-1/4"><div>{post.pwd}</div></td>
-                //   <td className="border-1 border-solid border-black w-1/4"><div>{post.access}</div></td>
-                // </tr>
-              
-                  // <tr className="border-1 border-solid border-black w-full">
-                  //   <td className="border-1 border-solid border-black w-1/4"><div>{post.Id}</div></td>
-                  //   <td className="border-1 border-solid border-black w-1/4"><div>{post.myusername}</div></td>
-                  //   <td className="border-1 border-solid border-black w-1/4"><div>{post.mypassword}</div></td>
-                  //   <td className="border-1 border-solid border-black w-1/4"><div>{post.myemail}</div></td>
-                  // </tr>
+                 
+                  <div className="flex flex-row w-full h-[3vh] bg-amber-100" key={post.Id}>
+                   <div className="border-1 border-solid border-black w-1/5 h-full text-xl text-black">{post.Id}</div>
+                   <div className="border-1 border-solid border-black w-1/5 h-full text-xl text-black">{post.usr}</div>
+                   <div className="border-1 border-solid border-black w-1/5 h-full text-xl text-black">{post.pwd}</div>
+                   <div className="border-1 border-solid border-black w-1/5 h-full text-xl text-black">{post.access}</div>
+                   <div className="border-1 border-solid border-black w-1/5 h-full flex flex-row">
+         
+                    
 
-                  <div className="flex flex-row w-full bg-amber-100" key={post.Id}>
-                   <div className="border-1 border-solid border-black w-1/5 text-xl text-black">{post.Id}</div>
-                   <div className="border-1 border-solid border-black w-1/5 text-xl text-black">{post.usr}</div>
-                   <div className="border-1 border-solid border-black w-1/5 text-xl text-black">{post.pwd}</div>
-                   <div className="border-1 border-solid border-black w-1/5 text-xl text-black">{post.access}</div>
-                   <div className="border-1 border-solid border-black w-1/5 text-xl text-black">Admin Tool</div>
+                                        
+                    <div className="mr-2 h-full w-1/2 flex flex-row justify-end">
+                     <div
+                      style={{
+                      position: "relative",
+                      width: "75%",
+                      height: "75%",
+                      }}
+                     >
+                      <Link href={"/edituser?id="+post.Id}  onClick={onEditIconClicked}>                       
+                        <Image
+                         src={pencil_icon}
+                         alt="pencil icon"
+                         fill = {true}
+                        />
+                      </Link>
+                     </div>
+                    </div>
+
+                   
+                    <div className="mr-2 h-full w-1/2 flex flex-row justify-start">
+                     <div
+                      style={{
+                      position: "relative",
+                      width: "75%",
+                      height: "75%",
+                      }}
+                     >
+                      <Link href={"/deleteuser?id="+post.Id} onClick={onDeleteIconClicked}>
+                       <Image
+                        src={trash_icon}
+                        alt="trash icon"
+                        fill = {true}
+                       />
+                      </Link>
+                     </div>
+                    </div>
+                    
+
+
+                   </div>
                   </div>
-                
-               ) 
-              )
+                 
+                 )        
+               )              
              }
-             {/* </table> */}
-            {/* </div> */}
-            </div>
+           </div>
           </div>
          </div>
-        </div>
+        </div> 
     );
 }
---
+++ app/deleteuser/layout.tsx
@@ -0,0 +1,24 @@
+//import Sidebar from "./sidebar.tsx";
+import RegisterScreen from "./userscreen";
+import Sidebar from "./sidebar";
+//import logo from "./images/new_logo.png";
+//import Image from "next/image";
+
+export default function BackOfficeLayout({ children }: {
+    children: React.ReactNode
+}) {
+ return (
+
+
+    <div className="h-full w-full flex flex-row justify-start border-1 border-black border-solid">
+      <div className="h-full w-1/4 flex flex-col justify-start border-1 border-black border-solid">
+       <Sidebar />
+      </div>
+      <div className="bg-primary h-full w-full text-white border-1 border-black border-solid">
+       <RegisterScreen />
+      </div>
+    </div>
+
+
+ );
+}
